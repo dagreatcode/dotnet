08afc7484a9022067b96a824d9b032c663cd3ef2
dagreatcode <dagreatcode@gmail.com>
2021-05-14 21:58:10 -0400

    made a get and print employee function in my program class. and added the main static to print the employees list.

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -5,40 +5,37 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<string> GetEmployees()
         {
-            Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
-                { "firstInning", 10 },
-                { "secondInning", 20},
-                { "thirdInning", 30},
-                { "fourthInning", 40},
-                { "fifthInning", 50}
-            };
-            Console.WriteLine("----------------");
-            Console.WriteLine("  Scoreboard");
-            Console.WriteLine("----------------");
-            Console.WriteLine("Inning |  Score");
-            Console.WriteLine("   1   |   {0}", myScoreBoard["firstInning"]);
-            Console.WriteLine("   2   |     {0}", myScoreBoard["secondInning"]);
-            Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
-            Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
-            Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
-
-            string[] favFoods = new string[3] { "pizza", "doughnuts", "icecream" };
-            string firstFood = favFoods[0];
-            string secondFood = favFoods[1];
-            string thirdFood = favFoods[2];
-            Console.WriteLine("I like {0}, {1}, and {2}", firstFood, secondFood, thirdFood);
-
-            List<string> employees = new List<string>() { "adam", "amy" };
-
-            employees.Add("barbara");
-            employees.Add("billy");
-            // Console.WriteLine("My employees include {0}, {1}, {2}, {3}", employees[0], employees[1], employees[2], employees[3]);
+            List<string> employees = new List<string>();
+            // Collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine();
+                // Break if the user hits ENTER without typing a name
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            // This is important!
+            return employees;
+        }
+        static void PrintEmployees(List<string> employees)
+        {
             for (int i = 0; i < employees.Count; i++)
             {
+
                 Console.WriteLine(employees[i]);
+
             }
+        }
+        static void Main(string[] args)
+        {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
